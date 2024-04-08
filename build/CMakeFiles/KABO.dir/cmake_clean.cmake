70e2a0496957726a95566c6d10e1a4ff58585fa2
LingNc <1020449099@qq.com>
2024-04-08 08:12:28 +0000

    first commit

---
+++ build/CMakeFiles/KABO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KABO.dir/main.cpp.o"
+  "KABO.pdb"
+  "KABO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KABO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    int a;
+    std::cin>>a;
+    puts("hello");
+    std::cout<<a;
+    return 0;
+}
