62f297785e8553d63349df17fb8ab547d7e739a3
LingNc <1020449099@qq.com>
2024-04-25 10:06:28 +0800

    sync

--- include/object/Game_Master.h
+++ include/object/Game_Master.h
@@ -1,7 +1,13 @@
 #include"Define.h"
+#include<vector>
 class Game_Master{
 public:
-
+    //构造函数
+    Game_Master();
+    Player& Now;
 private:
-    
+    std::vector<Player> _Playerarr;
+    New_Pile _NewPile;
+    Dis_Pile _DisPile;
+    Card _NewCard;
 };
